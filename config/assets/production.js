d1fa1e79f056ba5b2a45bf7c6c131afb9519af35
gavmac <gavinmacken87@gmail.com>
2017-02-12 12:15:46 -0500

    mongo_path

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,20 +4,20 @@ module.exports = {
   client: {
     lib: {
       css: [
-        'lib/bootstrap/dist/css/bootstrap.min.css',
-        '/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap.min.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
       ],
       js: [
-        '/lib/angular/angular.min.js',
-        '/lib/angular-resource/angular-resource.min.js',
-        '/lib/angular-animate/angular-animate.min.js',
-        '/lib/angular-ui-router/release/angular-ui-router.min.js',
-        '/lib/angular-ui-utils/ui-utils.min.js',
-        '/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        '/lib/angular-file-upload/angular-file-upload.min.js'
+        'public/lib/angular/angular.min.js',
+        'public/lib/angular-resource/angular-resource.min.js',
+        'public/lib/angular-animate/angular-animate.min.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+        'public/lib/angular-ui-utils/ui-utils.min.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+        'public/lib/angular-file-upload/angular-file-upload.min.js'
       ]
     },
-    css: '/dist/application.min.css',
-    js: '/dist/application.min.js'
+    css: 'public/dist/application.min.css',
+    js: 'public/dist/application.min.js'
   }
 };
